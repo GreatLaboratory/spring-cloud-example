53b5479d3a5e9476378bc7c1db99eba767cb0145
gm2202988 <kmk@gabia.com>
2021-05-20 09:13:39 +0900

    fix: port number | add: custom filter

--- apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -5,9 +5,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class FilterConfig {
-    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         // path에 있는 패턴으로 api-gateway에 클라이언트의 요청이 들어오면
         // uri에 있는 ip address와 port number인 micro service에 요청을 전달(route)한다.
@@ -16,7 +14,7 @@ public class FilterConfig {
                 .route(r -> r.path("/first-service/**")
                             .filters(f -> f.addRequestHeader("first-request", "first-request-header")
                                             .addResponseHeader("first-response", "first-response-header"))
-                            .uri("http://localhost:9001"))
+                            .uri("http://localhost:9090"))
                 .route(r -> r.path("/second-service/**")
                         .filters(f -> f.addRequestHeader("second-request", "second-request-header")
                                 .addResponseHeader("second-response", "second-response-header"))
